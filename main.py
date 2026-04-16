30daa01c35ccce2b71fa4c42b5825ea2a69d5ea7
Faraz <farazmoin.uk@gmail.com>
2026-04-16 13:29:14 -0400

    Help in use3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.write("I am Yamaan Faraz. Let me give you a little documentation")
 st.write("First, then type this in terminal:")
 st.code("pip install yf-calculator")
 st.write("Then, in your  file, write this:")
-st.code("from yf-calculator import yf-calculator-ui")
+st.code("from yf_calculator import yf_calculator-ui")
 st.write("Then, write:")
-st.code("yf-calculator-ui(csv_path='your .csv path with button goes here.',"
-        "text='your text in the About Us button', title='Your page title goes here.')")
+st.code("yf_calculator_ui(csv_path='your .csv path with button goes here.',"
+        "text='your text in the About Us button', title='Your page title goes here.', skip_first_column='Write True if want to skip, else, False.')")
